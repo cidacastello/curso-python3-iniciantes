280d8da38b9e2d0e8e1cef0e0b13846c55da74d7
cidarosa <lucita.rosa@gmail.com>
2024-04-30 18:34:29 -0300

    Aula 009 - Variáveis e constantes

---
+++ aulas/Aula009/main.py
@@ -0,0 +1,15 @@
+nome_completo = 'Maria da Silva'
+idade = 37
+peso = 68.450
+altura = 1.65
+quantidade_de_irmaos = 3
+quantidade_de_celulares = 2
+marca = 'Chevrolet'
+modelo = 'Onix'
+nome_do_cachorro = 'Marley'
+print('Nome:', nome_completo, ', idade:', idade)
+print('Peso:', peso, 'Altura:', altura)
+print(quantidade_de_irmaos, ' irmãos')
+print(quantidade_de_celulares, ' celulares')
+print('Marca do carro:', marca, 'Modelo:', modelo)
+print('Nome do pet:', nome_do_cachorro)

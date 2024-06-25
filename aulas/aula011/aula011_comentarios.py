b4ef9123febd9d685ca42ce099819e356fc30d64
cidarosa <lucita.rosa@gmail.com>
2024-06-24 21:00:14 -0300

    aula 011

---
+++ aulas/aula011/aula011_comentarios.py
@@ -0,0 +1,21 @@
+# Comentário em linha
+
+# declaração de variáveis
+# nome = 'Joaquim José da Silva Xavier'  # tipo string
+# idade = 48
+# altura = 1.72
+# saída de dados
+# print(nome, idade, altura)
+# print("Depois do comentário")
+
+'''
+    comentário em bloco
+    tudo o que está dentro das aspas
+    está comentado
+'''
+print('Depois do comentário')
+print("Ou")
+"""
+    aqui também é bloco de comentário
+    ou seja mais de uma linha
+"""
---
+++ aulas/aula011/aula011_declaracao_multipla.py
@@ -0,0 +1,14 @@
+# y = 15
+# x = 15
+# num = 15
+# declaração resumida
+y = x = num = 15
+print(y, x, num)  # saída de dados
+
+# declaração múltipla
+# nome = 'Joaquim José'
+# idade = 48
+# altura = 1.72
+nome, idade, altura = 'Joaquim Jose', 48, 1.72
+print(nome, idade, altura)
+

8b2081096c0d806430856491d04383161d129e33
cidarosa <lucita.rosa@gmail.com>
2024-08-14 18:20:49 -0300

    aula 13

---
+++ aulas/aula013/aula013.py
@@ -0,0 +1,19 @@
+print('Programa para calcular a área de um terreno retangular', '\n')
+# area = base * altura
+
+# entrada de dados
+base = float(input('Informe o valor da largura do terreno: '))
+altura = float(input('Informe o valor da profundidade do terreno: '))
+
+# cálculo da área - processamento
+area = base * altura
+
+# saída de dados
+# print('A área do terreno é de', area, 'm2')
+print('A área do terreno é de ' + str(area) + ' m2')
+
+# nome = 'Cida'
+# sobrenome = 'Castello'
+# print(nome, sobrenome)
+# concatenar => juntar strings, textos
+# print(nome + ' '+ sobrenome)

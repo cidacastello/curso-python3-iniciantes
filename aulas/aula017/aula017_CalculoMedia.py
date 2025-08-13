c20506a310933094f7b2a1757aedb719d2b3ea9b
cidarosa <lucita.rosa@gmail.com>
2025-08-13 18:51:48 -0300

    Add workflow PR e aula 17

---
+++ aulas/aula017/aula017_CalculoMedia.py
@@ -0,0 +1,16 @@
+nota1 = float(input('Informe o valor da primeira nota: '))
+nota2 = float(input('Informe o valor da segunda nota: '))
+
+# cálculo da média
+media = (nota1 + nota2) /2
+
+# Variáveis de tipo lógico também podem ser utilizadas
+# para armazenar o resultado de expressões e comparações.
+# variável tipo lógoco
+
+# aprovado = media >= 7.0
+
+# print(f'Aluno aprovado? {aprovado}')
+
+# ou
+print(f'Aluno aprovado? {media >= 7.0}')
---
+++ aulas/aula017/aula017_operadores_relacionais.py
@@ -0,0 +1,28 @@
+# Op    Operação         Matemática
+# ==    igualdade           =
+# >     maior que           >
+# <     menor que           <
+# !=     diferente          ≠
+# >=     maior ou igual     ≥
+# <=     menor ou igual     ≤
+
+# Exemplo de uso de operadores relacionais
+
+a = 1
+b = 5
+c = 2
+d = 1
+
+print(f'a é igual a b ? {a == b}')
+print(f'b é maior que a? {b > a}')
+print(f'a é menor que b? {a < b}')
+print(f'a é igual a d? {a == d}')
+print(f'b é maior ou igual a a? {b >= a}')
+print(f'c é menor ou igual a b? {c <= b}')
+print(f'd é diferente de a? {d != a}')
+print(f'd é diferente de b? {d != b}')
+
+
+
+
+

2620ba59688e350c4a90ee4d44650161ab135656
cidarosa <lucita.rosa@gmail.com>
2025-08-18 20:48:02 -0300

    aula 18 - operadores lógicos

---
+++ aulas/aula018_OperadoresLogicos/aula018_OperadoresLogicos.py
@@ -0,0 +1,20 @@
+#  Operador | Operação | Precedência |
+#  not      | não      | 1ª          |
+#  and      | e        | 2ª          |
+#  or       | ou       | 3ª          |
+
+print('\nNOT')
+print(not True)
+print(not False)
+
+print('\nAND')
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print('\nOR')
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
